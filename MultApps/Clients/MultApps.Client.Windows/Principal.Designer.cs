ad8e17b1401a81798a33004fdb4ad72386e86efc
Yuri Costa e Silva Eduardo <yuri.tag145@gmail.com>
2022-09-05 21:36:31 -0300

    Inicialização do sistema com Gitignore visual studio

--- MultApps/Clients/MultApps.Client.Windows/Principal.Designer.cs
+++ MultApps/Clients/MultApps.Client.Windows/Principal.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(733, 450);
+            this.ClientSize = new System.Drawing.Size(704, 450);
             this.Name = "Principal";
             this.Text = "Principal";
             this.ResumeLayout(false);
